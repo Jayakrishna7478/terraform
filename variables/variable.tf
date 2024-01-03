280e863954ef45dbf52fa5295d62c635937cf341
Jayakrishna7478 <jayakrishnachowdry123@gmail.com>
2024-01-03 17:44:15 +0530

    terraform

---
+++ variables/variable.tf
@@ -0,0 +1,16 @@
+variable "iam_id" {
+  type = string
+  default = "ami-03265a0778a880afb"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
+
+variable "tags" {
+  type = map
+  default = {
+    Name = "jaya"
+  }
+}
