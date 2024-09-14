a3dc9ac777b83f5acc537c3309afe089bb69ac8f
Jayakrishna7478 <jayakrishnachowdry123@gmail.com>
2024-09-14 14:54:27 +0530

    jenkins

--- count/variables.tf
+++ count/variables.tf
@@ -1,17 +1,17 @@
 variable "instance_names" {
   type = map
   default = {
-    mongodb = "t3.small"
+    #mongodb = "t3.small"
     redis = "t2.micro"
-    mysql = "t3.small"
-    rabbitmq = "t2.micro"
-    catalogue = "t2.micro"
-    cart = "t2.micro"
-    user = "t2.micro"
-    shipping = "t3.small"
-    payment = "t2.micro"
-    dispatch = "t2.micro"
-    web = "t2.micro"
+    # mysql = "t3.small"
+    # rabbitmq = "t2.micro"
+    # catalogue = "t2.micro"
+    # cart = "t2.micro"
+    # user = "t2.micro"
+    # shipping = "t3.small"
+    # payment = "t2.micro"
+    # dispatch = "t2.micro"
+    # web = "t2.micro"
   }
 }
 
